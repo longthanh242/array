6f004ec671685200edfa54beabd0ce705dd89a9e
longthanh242 <155937409+longthanh242@users.noreply.github.com>
2024-05-24 15:53:38 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,113 @@
+// Bài 1: Viết một chương trình để in các ký tự trong một mảng theo thứ tự đảo ngược.
+// Ví dụ, nếu một mảng chứa các ký tự [ 'c', 's', 'c', '2', '6', '1' ]sau khi thực hiện đảo ngược sẽ là "162csc".
+
+
+
+        // let arr = ['c', 's', 'c', '2', '6', '1']
+        // console.log(arr.reverse());
+        // console.log(arr.join(''));
+
+
+
+// Bài 2: Viết một chương trình nhận một số nhập vào và chèn dấu (-) giữa 2 số chẵn.
+// Ví dụ nếu bạn nhập vào 245468 thì kết quả của chương trình sẽ là 2-454-6-8.
+
+
+
+        // let number = +prompt("Nhập vào 1 số: ")
+        // let arr = number.toString().split('');
+        // console.log(arr);
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i]%2==0 && arr[i+1]%2==0) {
+        //         arr.splice(i+1,0,'-')
+        //     }
+        // }
+        // console.log(arr.join(''));
+
+
+
+// Bài 3: Cho 1 một mảng  let  numbers=[1,5,8,7,9,6,5,41,2,6,55,4,8];
+// Tìm số lớn nhất trong mảng
+
+
+
+        // let numbers=[1,5,8,7,9,6,5,41,2,6,55,4,8]
+        // let max = numbers[0];
+        // for (let i = 1; i < numbers.length; i++) {
+        //     if (numbers[i]>max) {
+        //         max = numbers[i];
+        //     }
+        // }
+        // document.write("Số lớn nhất trong mảng là:", max)
+
+
+
+// Bài 4: Viết một chương trình nhập vào một chuỗi 
+// và chuyển các ký tự chữ thường trong chuỗi vừa nhập sang thành dạng chữ hoa và ngược lại
+// Ví dụ: nếu bạn nhập vào chuỗi 'Keep Calm And Code On' kết quả của chương trình là 'kEEP cALM aND cODE oN'.
+
+        
+
+        // let string = prompt("Nhập vào 1 chuỗi chữ cái:");
+        // let string1 = '';
+
+        // for (let i = 0; i < string.length; i++) {
+        //         let j = string[i];
+        //         if (j === j.toLowerCase()) {
+        //                 string1 += j.toUpperCase();
+        //         } else {
+        //                 string1 += j.toLowerCase();
+        //         }
+        // }
+        // console.log(string1);
+
+
+
+        
+        // let string = prompt("Nhập vào 1 chuỗi chữ cái:")
+        // let string1 = string.split('')
+        // console.log(string1);
+        // console.log(string1.length);
+        // for (let i = 0; i < string1.length; i++) {
+        //         // console.log(typeof string1[i]);
+        //     let j = string1[i];
+
+        //     if (j === j.toLowerCase()) {
+        //         // console.log(j === j.toLowerCase());
+        //         // j = j.toUpperCase();
+        //         // j.toUpperCase()
+        //                 string1.splice(i,1,j.toUpperCase());
+        //      } else {
+        //                 string1.splice(i,1,j.toLowerCase());
+        //         // j=j.toLowerCase();
+        //     }
+        // }
+        // document.write(string1.join(''))
+
+
+
+
+// Bài 5: Cho 1 một mảng let numbers=[1,5,8,7,9,6,5,41,2,6,55,4,8];
+// Sắp xếp các phần tử trong mảng theo thứ tự tăng dần.
+
+        // function hamSoSanh(a, b) {
+        //         if (a>b) {
+        //                 return 1
+        //         } else if (a<b) {
+        //                 return -1
+        //         }
+        //         return 0
+        // }
+        
+        // let numbers=[1,5,8,7,9,6,5,41,2,6,55,4,8];
+        // document.write(numbers.sort(hamSoSanh));
+        
+
+        
+
+        
+
+
+        
+        
+
